90a20368508d5883b406cda1b23dfcb2842b0d74
Grace Yong <grace.yong.xue.min@gmail.com>
2023-01-15 03:22:59 +0800

    minor edits

--- lib/pages/alarm_page.dart
+++ lib/pages/alarm_page.dart
@@ -1,128 +1,116 @@
-// import 'dart:async';
-// import 'dart:developer';
+import 'dart:async';
+import 'dart:developer';
 
-// import 'package:alarming/pages/choose_alarm_page.dart';
-// import 'package:flutter/material.dart';
-// import 'package:alarm/alarm.dart';
+import 'package:alarming/pages/choose_alarm_page.dart';
+import 'package:flutter/material.dart';
+import 'package:alarm/alarm.dart';
 
-// class AlarmPage extends StatefulWidget {
-//   const AlarmPage({super.key, required this.name, required this.default_alarm});
-//   final String name;
-//   final String default_alarm;
+class AlarmPage extends StatefulWidget {
+  const AlarmPage({super.key, required this.name, required this.default_alarm});
+  final String name;
+  final String default_alarm;
 
-//   @override
-//   State<AlarmPage> createState() => _AlarmPageState();
-// }
+  @override
+  State<AlarmPage> createState() => _AlarmPageState();
+}
 
-// class _AlarmPageState extends State<AlarmPage> {
-//   // record wake up time
-//   final timePickerController = TextEditingController();
-//   TimeOfDay wakeUpTime = TimeOfDay.now();
-//   bool isRinging = false;
+class _AlarmPageState extends State<AlarmPage> {
+  // record wake up time
+  final timePickerController = TextEditingController();
+  TimeOfDay wakeUpTime = TimeOfDay.now();
+  bool isRinging = false;
 
-//   Future displayTimePicker(BuildContext context) async {
-//     var time = await showTimePicker(context: context, initialTime: wakeUpTime);
+  Future displayTimePicker(BuildContext context) async {
+    var time = await showTimePicker(context: context, initialTime: wakeUpTime);
 
-//     if (time != null) {
-//       setState(() {
-//         timePickerController.text = "${time.hour}:${time.minute}";
-//         wakeUpTime = time;
-//       });
-//     }
+    if (time != null) {
+      setState(() {
+        timePickerController.text = "${time.hour}:${time.minute}";
+        wakeUpTime = time;
+      });
+    }
 
-//     //createTimer(wakeUpTime);
+    final now = DateTime.now();
+    DateTime dt = DateTime(
+      now.year,
+      now.month,
+      now.day,
+      wakeUpTime.hour,
+      wakeUpTime.minute,
+    );
 
-//     final now = DateTime.now();
-//     DateTime dt = DateTime(
-//       now.year,
-//       now.month,
-//       now.day,
-//       wakeUpTime.hour,
-//       wakeUpTime.minute,
-//     );
+    setAlarm(dt);
+  }
 
-//     setAlarm(dt);
-//   }
+  Future<void> setAlarm(DateTime dateTime) async {
+    await Alarm.set(
+      alarmDateTime: dateTime,
+      assetAudio: 'assets/fireremix.mp3',
+      loopAudio: true,
+      onRing: () {
+        setState(() {
+          isRinging = true;
+        });
 
-//   // void createTimer(TimeOfDay wakeUpTime) {
-//   //   int hours = wakeUpTime.hour + 24 - TimeOfDay.now().hour;
-//   //   int mins = wakeUpTime.minute + 60 - TimeOfDay.now().minute;
-//   //   final duration = Duration(hours: hours, minutes: mins);
-//   //   Timer(duration, alarm);
-//   // }
+        stopAlarm();
+      },
+      notifTitle: 'Alarm notification',
+      notifBody: 'Your alarm is ringing',
+    );
+  }
 
-//   // // create alarm
-//   // void alarm() {}
+  stopAlarm() {
+    // set up the button
+    Widget stopButton = TextButton(
+      child: Text("Stop"),
+      onPressed: () {
+        Alarm.stop();
+        setState(() {
+          isRinging = false;
+        });
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ChooseAlarmPage(name: widget.name)));
+      },
+    );
 
-//   Future<void> setAlarm(DateTime dateTime) async {
-//     await Alarm.set(
-//       alarmDateTime: dateTime,
-//       assetAudio: 'assets/fireremix.mp3',
-//       loopAudio: true,
-//       onRing: () {
-//         setState(() {
-//           isRinging = true;
-//         });
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: const Text("Alarm notification"),
+      content: const Text("Your alarm is ringing"),
+      actions: [
+        stopButton,
+      ],
+    );
 
-//         stopAlarm();
-//       },
-//       notifTitle: 'Alarm notification',
-//       notifBody: 'Your alarm is ringing',
-//     );
-//   }
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
 
-//   stopAlarm() {
-//     // set up the button
-//     Widget stopButton = TextButton(
-//       child: Text("Stop"),
-//       onPressed: () {
-//         Alarm.stop();
-//         setState(() {
-//           isRinging = false;
-//         });
-//         Navigator.of(context).push(MaterialPageRoute(
-//             builder: (context) => ChooseAlarmPage(name: widget.name)));
-//       },
-//     );
-
-//     // set up the AlertDialog
-//     AlertDialog alert = AlertDialog(
-//       title: Text("Alarm notification"),
-//       content: Text("Your alarm is ringing"),
-//       actions: [
-//         stopButton,
-//       ],
-//     );
-
-//     // show the dialog
-//     showDialog(
-//       context: context,
-//       builder: (BuildContext context) {
-//         return alert;
-//       },
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Alarm Page"),
-//       ),
-//       body: Center(
-//         child: Column(
-//           children: [
-//             TextFormField(
-//               controller: timePickerController,
-//               decoration: const InputDecoration(
-//                   labelText: 'Time picker', border: OutlineInputBorder()),
-//             ),
-//             ElevatedButton(
-//                 onPressed: () => displayTimePicker(context),
-//                 child: const Text("Pick Time"))
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alarm Page"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: timePickerController,
+              decoration: const InputDecoration(
+                  labelText: 'Time picker', border: OutlineInputBorder()),
+            ),
+            ElevatedButton(
+                onPressed: () => displayTimePicker(context),
+                child: const Text("Pick Time"))
+          ],
+        ),
+      ),
+    );
+  }
+}
