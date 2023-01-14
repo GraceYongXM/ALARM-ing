52785917c6bce1964719d690d938082c3a30549d
Grace Yong <grace.yong.xue.min@gmail.com>
2023-01-15 03:14:29 +0800

    find next alarm

--- lib/pages/choose_alarm_page.dart
+++ lib/pages/choose_alarm_page.dart
@@ -1,14 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChooseAlarmPage extends StatefulWidget {
-  const ChooseAlarmPage({super.key});
+  const ChooseAlarmPage({super.key, required this.name});
+  final String name;
 
   @override
   State<ChooseAlarmPage> createState() => _ChooseAlarmPageState();
 }
 
 class _ChooseAlarmPageState extends State<ChooseAlarmPage> {
-  var friend;
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  String nextAlarmUser = "NO ONE";
+  List<Map<String, dynamic>> userAndTime = [];
+
+  Future<void> sortUsersByAlarm() async {
+    return db
+        .collection("users")
+        .orderBy("alarm_time")
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        Map<String, dynamic> row = {
+          'username': doc['name'],
+          'alarmTime': doc['alarm_time'],
+        };
+        userAndTime.add(row);
+      });
+    });
+  }
+
+  void getNextAlarmUser() {
+    sortUsersByAlarm();
+    for (int i = 0; i < userAndTime.length - 1; i++) {
+      if (widget.name == userAndTime[i]["username"]) {
+        nextAlarmUser = userAndTime[i + 1]["username"];
+        break;
+      }
+    }
+  }
 
   String dropdownvalue = 'Alarm 1';
 
@@ -31,7 +61,7 @@ class _ChooseAlarmPageState extends State<ChooseAlarmPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Choose alarm for $friend"),
+            Text("Choose alarm for $nextAlarmUser"),
             DropdownButton(
               // Initial Value
               value: dropdownvalue,
