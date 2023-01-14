cc60e25ed9ab36a237db5392f9898c6e3922ebf1
Jamie Lee Fang Hua <jamie.lfh@u.nus.edu>
2023-01-14 14:51:18 +0800

    linked firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
 
 void main() {
   runApp(const MyApp());
