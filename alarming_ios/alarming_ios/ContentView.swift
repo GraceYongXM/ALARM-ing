bb8b1775e7607aaf55fefc445352923783721de9
Jamie Lee Fang Hua <jamie.lfh@u.nus.edu>
2023-01-14 13:34:41 +0800

    new IOS project

---
+++ alarming_ios/alarming_ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  alarming_ios
+//
+//  Created by Jamie Lee on 14/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alarming_ios/alarming_ios/alarming_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  alarming_iosApp.swift
+//  alarming_ios
+//
+//  Created by Jamie Lee on 14/1/23.
+//
+
+import SwiftUI
+
+@main
+struct alarming_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
